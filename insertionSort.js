ad47e4a56f439001108b264b45e985eb65acfa09
Grant Keith <grant.keith@mail.mcgill.ca>
2020-01-16 21:25:26 -0500

    initial algorithm

--- insertionSort.js
+++ insertionSort.js
@@ -1,3 +1,16 @@
-function test(){
-    document.write("Hello World!");
+var A = [1,3,5,4,2,6];
+function insertionSort(A){
+    var key;
+    var i;
+    var j;
+    for(j = 1; j <= A.length; j++){
+        key = A[j];
+        i = j-1;
+        while(i>0 && A[i]>key){
+            A[i+1] = A[i];
+            i--;
+        }
+        A[i+1] = key;
+    }
+    
 }
